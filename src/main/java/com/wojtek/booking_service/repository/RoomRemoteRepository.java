6fa654f89f37546aa14c32f79a4c7bfdb70dd717
WojciechWeg <wojtek.weg@gmail.com>
2020-11-20 13:05:53 +0100

    hystrix CB basic application

--- src/main/java/com/wojtek/booking_service/repository/RoomRemoteRepository.java
+++ src/main/java/com/wojtek/booking_service/repository/RoomRemoteRepository.java
@@ -1,6 +1,9 @@
 package com.wojtek.booking_service.repository;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wojtek.booking_service.dto.Room;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Service;
@@ -13,8 +16,16 @@ public class RoomRemoteRepository {
     @Autowired
     private RoomServiceClient roomServiceClient;
 
+    @HystrixCommand(fallbackMethod = "getFallbackRoom")
     public Optional<Room> findById(String roomName) {
         Room room = roomServiceClient.getRoom(roomName);
+        return Optional.ofNullable(room);
+    }
+
+    public Optional<Room> getFallbackRoom(String roomName){
+        System.out.println("Circuit breaker went on");
+        Room room = new Room();
+        room.setName("Fallback room " + roomName);
         return Optional.ofNullable(room);
     }
 }
